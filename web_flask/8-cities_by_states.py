b067cfedefc2ed99ba58b300779d75e9a0532310
Ignacio Chitnisky <elinmortal@gmail.com>
2021-09-12 22:59:24 +0000

    adding 8

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""a script that starts a Flask web application"""
+
+from models import storage
+from models.state import State
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def states():
+    """Returns a html listing the states"""
+    return render_template('8-cities_by_states.html',
+                           states=storage.all('State').values())
+
+
+@app.teardown_appcontext
+def teardown(self):
+    """remove the current SQLAlchemy Session"""
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
