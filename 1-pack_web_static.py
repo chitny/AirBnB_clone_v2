2a6df81cc514df0d3ceafc1df1b991838bf27d9b
Ignacio Chitnisky <elinmortal@gmail.com>
2022-01-17 02:01:31 +0000

    fixing 1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,18 +1,11 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
 """1. Compress before sending"""
 
 from fabric.operations import local
-=======
-'''Fabric script that generates a .tgz archive'''
-
-from fabric.api import run, local
->>>>>>> cd4c93e33e47d8d47247bb74daae22eff0d3b36b
 from datetime import datetime
 
 
 def do_pack():
-<<<<<<< HEAD
     """Function to compress files"""
     local("mkdir -p versions")
     result = local("tar -cvzf versions/web_static_{}.tgz web_static"
@@ -21,14 +14,3 @@ def do_pack():
     if result.failed:
         return None
     return result
-=======
-    '''for the tgz'''
-    try:
-        local('mkdir -p versions')
-        d_t = datetime.now().strftime("%Y%m%d%H%M%S")
-        filename = "versions/web_static_" + d_t + ".tgz"
-        local("tar -cvzf" + filename + " web_static")
-        return filename
-    except:
-        return None
->>>>>>> cd4c93e33e47d8d47247bb74daae22eff0d3b36b
